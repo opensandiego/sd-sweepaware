c62353b6d2c5afbdbf6313dd7e3cf73b84011440
Stephen Lester <stephen@lester.io>
2015-08-21 19:55:40 -0700

    Add MongoDB modules

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+// database connections
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/sweepaware');
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
@@ -21,6 +26,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// give each request access to the database
+app.use(function(req, res, next) {
+    req.db = db;
+    next();
+});
 
 app.use('/', routes);
 app.use('/users', users);
